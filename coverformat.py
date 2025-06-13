873e69452ffc0a70b6100a76ccec7d58be8bad40
febedang13 <phoebe.d.dang@gmail.com>
2025-06-13 18:47:38 -0400

    add comments

--- coverformat.py
+++ coverformat.py
@@ -4,6 +4,16 @@ from colorthief import ColorThief
 import io
 
 def get_dominant_color(url):
+    """
+    Get dominant color from image URL.
+    
+    Parameters: 
+        url (string): Image URL
+    
+    Returns:
+        dominant_color (tuple): (r, g, b)
+    """
+
     file_data = urlopen(url)
     f = io.BytesIO(file_data.read())
     color_thief = ColorThief(f)
@@ -11,15 +21,36 @@ def get_dominant_color(url):
     return dominant_color
 
 def image_to_bytes(image):
+    """
+    Converts PIL image to bytes.
+    
+    Parameters: 
+        image (Image): PIL image
+    
+    Returns:
+        bytes: bytestring of image
+    """
+
     image_bytes = io.BytesIO()
     image.save(image_bytes, format="PNG")
     return image_bytes.getvalue()
 
 def create_cover(url):
+    """
+    Formats image as Notion cover.
+    
+    Parameters: 
+        url (string): Image URL
+    
+    Returns:
+        bytes: bytestring of formatted image
+    """
+
     cover_color = get_dominant_color(url)
     img = Image.open(urlopen(url))
     padded_img = ImageOps.pad(img, (1500, 600), color=cover_color)
     return image_to_bytes(padded_img)
+
 
 # url = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1589286085i/48677123.jpg"
 # create_cover(url).show()
--- notionservice.py
+++ notionservice.py
@@ -10,6 +10,13 @@ load_dotenv()
 notion = Client(auth=os.environ["NOTION_KEY"])
 
 def create_file_upload():
+    """
+    Creates Notion File Upload object.
+    
+    Returns:
+        file_upload_id (string): file upload id
+    """
+
     file_create_response = requests.post("https://api.notion.com/v1/file_uploads", headers={
         "Authorization": f"Bearer {os.environ["NOTION_KEY"]}",
         "accept": "application/json",
@@ -27,6 +34,14 @@ def create_file_upload():
 
 
 def send_file_upload(upload_id, cover_file):
+    """
+    Uploads file contents to Notion.
+    
+    Parameters: 
+        upload_id (string): file upload id
+        cover_file (bytes): binary file contents
+    """
+
     files = {
         "file": ("cover.png", cover_file, "image/png")
     }
@@ -44,10 +59,16 @@ def send_file_upload(upload_id, cover_file):
         raise Exception(
             f"File upload failed with status code {response.status_code}: {response.text}")
 
-    return response.text
-
 
 def update_cover(file_upload_id, page_id):
+    """
+    Updates Notion page cover.
+    
+    Parameters: 
+        file_upload_id (string): file upload id
+        page_id (string): Notion page id
+    """
+
     update_response = notion.pages.update(
         **{
             "page_id": page_id,
@@ -63,6 +84,13 @@ def update_cover(file_upload_id, page_id):
 
 
 def update_notion_books(page):
+    """
+    Updates Notion book page.
+    
+    Parameters: 
+        page: Notion block
+    """
+
     if page['cover']['type'] == 'external':
         page_id = page['id']
         url = page['cover']['external']['url']
@@ -73,11 +101,14 @@ def update_notion_books(page):
         update_cover(file_upload_id, page_id)
 
 
+# # Iterate through all pages in book database
 # for block in iterate_paginated_api(
 #     notion.databases.query, database_id=os.environ["NOTION_DATABASE_ID"]
 # ):
 #     update_notion_books(block)
 
+
+# Update book pages created after date
 response = notion.databases.query(
     **{
         "database_id": os.environ["NOTION_DATABASE_ID"],
@@ -89,5 +120,6 @@ response = notion.databases.query(
         }
     }
 )
-result = response["results"][0]
-update_notion_books(result)
+
+for block in response['results']:   
+    update_notion_books(block)
